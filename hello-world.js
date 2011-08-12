bd268d70cd71ea90e53f6074b29eb5f525ef6193
Carlos Afanador <carlos.afanador@originaction.com>
2011-08-12 14:30:45 -0500

    Don't know why.

--- hello-world.js
+++ hello-world.js
@@ -1,7 +1,4 @@
 alert ("why hello there");
 
 //add our first array here
-var firstArray=new Array();
-firstArray[0]="First thing";
-firstArray[1]="Second thing";
-firstArray[2]="Third thing";
+var firstArray=new Array("First thing","Second thing","Third thing");
